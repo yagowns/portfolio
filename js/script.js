25048d73b4593e2ef32566d54f2cf35ba4362ce8
Yago <yagowns@gmail.com>
2019-03-02 18:42:27 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,8 +13,9 @@ var AboutMe = Barba.BaseView.extend({
 	// The new Container is ready and attached to the DOM.
 	//Change words on about page
 	var divs = $('span[id^="content-"]').hide();
-	i = 0 ++i % divs.length;
-	divs.eq(i).animate({width:'toggle'},350).fadeIn(400).delay(2000).fadeOut(400);
+	i = 1;
+	divs.eq(i).animate({width:'toggle'},350,cycle).fadeIn(400).delay(2000).fadeOut(400);
+	i = ++i % divs.length;
 	},
 	onEnterCompleted: function() {
 	// The Transition has just finished.
