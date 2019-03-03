2b46bb0974ec19fd4df2533b1b5b20c06a445284
Yago <yagowns@gmail.com>
2019-03-03 17:07:43 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,15 +11,15 @@ var Homepage = Barba.BaseView.extend({
 	namespace: 'home',
 	onEnter: function() {
 	// The new Container is ready and attached to the DOM.
-	$('.barba-container').addClass('animated slideInLeft fast');
+	$('.barba-container').addClass('animated fadeIn fast');
 	},
 	onEnterCompleted: function() {
 	// The Transition has just finished.
 	},
 	onLeave: function() {
 	// A new Transition toward a new page has just started.
-	$('.barba-container').removeClass('animated slideInLeft fast');
-	$('.barba-container').addClass('animated  slideOutRight fast');
+	$('.barba-container').removeClass('animated fadeIn fast');
+	$('.barba-container').addClass('animated  fadeOut fast');
 	},
 	onLeaveCompleted: function() {
 	// The Container has just been removed from the DOM.
@@ -32,8 +32,8 @@ var AboutMe = Barba.BaseView.extend({
 	namespace: 'about',
 	onEnter: function() {
 	// The new Container is ready and attached to the DOM.
-	$('.barba-container').removeClass('animated slideOutRight fast');
-	$('.barba-container').addClass('animated slideIntRight fast');
+	$('.barba-container').removeClass('animated fadeOut fast');
+	$('.barba-container').addClass('animated fadeIn fast');
 	//Change words on about page
 	var divs = $('span[id^="content-"]').hide();
 	i = 0;
