071885293ca5e73396ce1227e8afdcab343c22fc
Yago <yagowns@gmail.com>
2019-03-03 15:57:11 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ var AboutMe = Barba.BaseView.extend({
 	namespace: 'about',
 	onEnter: function() {
 	// The new Container is ready and attached to the DOM.
+	$('#barba-wrapper').fadeIn(400);
 	//Change words on about page
 	var divs = $('span[id^="content-"]').hide();
 	i = 0;
@@ -47,6 +48,133 @@ var AboutMe = Barba.BaseView.extend({
 });
 // Don't forget to init the view!
 AboutMe.init();
+
+var Work = Barba.BaseView.extend({
+	namespace: 'work',
+	onEnter: function() {
+	// The new Container is ready and attached to the DOM.
+	//hover change image for the portfolio showcase
+	$('#one').hover(function () {
+		$('.empty_right').css({'background-image': 'url("img/alivecash2.gif")'});
+	});
+
+	$('#two').hover(function () {
+		$('.empty_right').css({'background-image': 'url("img/purge2.gif")'});
+	});
+
+	$('#three').hover(function () {
+		$('.empty_right').css({'background-image': 'url("img/alivecash.png")'});
+	});
+
+	$('#four').hover(function () {
+		$('.empty_right').css({'background-image': 'url("img/alivecash.png")'});
+	});
+
+	$('#five').hover(function () {
+		$('.empty_right').css({'background-image': 'url("img/alivecash.png")'});
+	});
+
+	//hover change for client logos
+	$('#aerosoles').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/aerosoles.png")'});
+	});
+
+	$('#azoris').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/azoris.png")'});
+	});
+
+	$('#cci').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/cci.png")'});
+	});
+
+	$('#nutella').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/nutella.png")'});
+	});
+
+	$('#tendam').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/tendam.png")'});
+	});
+
+	$('#cortefiel').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/cortefiel.png")'});
+	});
+
+	$('#pedrodelhierro').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/pedrodelhierro.png")'});
+	});
+
+	$('#springfield').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/springfield.png")'});
+	});
+
+	$('#womensecret').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/womensecret.png")'});
+	});
+
+	$('#diadermine').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/diadermine.png")'});
+	});
+
+	$('#schwarzkopf').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/schwarzkopf.png")'});
+	});
+
+	$('#syoss').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/syoss.png")'});
+	});
+
+	$('#bayou').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/bayou.png")'});
+	});
+
+	$('#cointreau').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/cointreau.png")'});
+	});
+
+	$('#fiuza').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/fiuza.png")'});
+	});
+
+	$('#glenfiddich').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/glenfiddich.png")'});
+	});
+
+	$('#hendricks').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/hendricks.png")'});
+	});
+
+	$('#grous').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/grous.png")'});
+	});
+
+	$('#valbom').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/valbom.png")'});
+	});
+
+	$('#fornodeminas').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/fornodeminas.png")'});
+	});
+
+	$('#gatorade').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/gatorade.png")'});
+	});
+
+	$('#nike').hover(function () {
+		$('.logos_rec').css({'background-image': 'url("img/nike.png")'});
+	});
+	},
+	onEnterCompleted: function() {
+	// The Transition has just finished.
+	},
+	onLeave: function() {
+	// A new Transition toward a new page has just started.
+	},
+	onLeaveCompleted: function() {
+	// The Container has just been removed from the DOM.
+	}
+});
+// Don't forget to init the view!
+Work.init();
 
 //home and about circle showcase
 var showcaseArray = [
@@ -75,119 +203,6 @@ $(document).ready(function() {
   	$('#showcase').removeClass('animate').addClass('animate');
   	$('#showcase').attr({src: randomShowcase});
   }, 5000);
-});
-
-//hover change image for the portfolio showcase
-$('#one').hover(function () {
-	$('.empty_right').css({'background-image': 'url("img/alivecash2.gif")'});
-});
-
-$('#two').hover(function () {
-	$('.empty_right').css({'background-image': 'url("img/purge2.gif")'});
-});
-
-$('#three').hover(function () {
-	$('.empty_right').css({'background-image': 'url("img/alivecash.png")'});
-});
-
-$('#four').hover(function () {
-	$('.empty_right').css({'background-image': 'url("img/alivecash.png")'});
-});
-
-$('#five').hover(function () {
-	$('.empty_right').css({'background-image': 'url("img/alivecash.png")'});
-});
-
-
-
-
-//hover change for client logos
-$('#aerosoles').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/aerosoles.png")'});
-});
-
-$('#azoris').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/azoris.png")'});
-});
-
-$('#cci').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/cci.png")'});
-});
-
-$('#nutella').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/nutella.png")'});
-});
-
-$('#tendam').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/tendam.png")'});
-});
-
-$('#cortefiel').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/cortefiel.png")'});
-});
-
-$('#pedrodelhierro').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/pedrodelhierro.png")'});
-});
-
-$('#springfield').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/springfield.png")'});
-});
-
-$('#womensecret').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/womensecret.png")'});
-});
-
-$('#diadermine').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/diadermine.png")'});
-});
-
-$('#schwarzkopf').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/schwarzkopf.png")'});
-});
-
-$('#syoss').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/syoss.png")'});
-});
-
-$('#bayou').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/bayou.png")'});
-});
-
-$('#cointreau').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/cointreau.png")'});
-});
-
-$('#fiuza').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/fiuza.png")'});
-});
-
-$('#glenfiddich').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/glenfiddich.png")'});
-});
-
-$('#hendricks').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/hendricks.png")'});
-});
-
-$('#grous').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/grous.png")'});
-});
-
-$('#valbom').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/valbom.png")'});
-});
-
-$('#fornodeminas').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/fornodeminas.png")'});
-});
-
-$('#gatorade').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/gatorade.png")'});
-});
-
-$('#nike').hover(function () {
-	$('.logos_rec').css({'background-image': 'url("img/nike.png")'});
 });
 
 //animation//
