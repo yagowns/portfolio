4ddb24d53ac5807052b738e559c61efa496ff665
Yago <yagowns@gmail.com>
2019-03-05 16:44:23 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ var Work = Barba.BaseView.extend({
 	$('.top_left').addClass('animated fadeInDown');
 	$('#work_about').addClass('animated fadeIn slow');
 	$('.section').addClass('animated fadeIn stepone');
-	$('.white_left').addClass('animated SlideInLeft faster');
+	$('.white_left').addClass('animated slideInLeft faster');
 	$('#one').addClass('animated fadeIn');
 	$('#two').addClass('animated fadeIn stepone');
 	$('#three').addClass('animated fadeIn slow');
