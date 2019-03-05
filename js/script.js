bdffb43be280c9a433ff5ecf4dc735216b8daaa2
Yago <yagowns@gmail.com>
2019-03-05 09:44:46 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,10 +32,10 @@ var AboutMe = Barba.BaseView.extend({
 	$('.top_left').addClass('animated fadeInDown');
 	$('#home_about').addClass('animated fadeIn slow');
 	$('#work_about').addClass('animated fadeIn slow');
-	$('#first').addClass('animated fadeInDown');
-	$('#second').addClass('animated fadeInDown stepone');
-	$('#third').addClass('animated fadeInDown slow');
-	$('#fourth').addClass('animated fadeInDown steptwo');
+	$('#first').addClass('animated fadeInLeft');
+	$('#second').addClass('animated fadeInLeft stepone');
+	$('#third').addClass('animated fadeInLeft slow');
+	$('#fourth').addClass('animated fadeInLeft steptwo');
 	//Expanding Logo
 	$('.top_left').mouseover(function () {
 	$('#yagomurakami').text('Yago Murakami');
