4a691d429b1ec12de26e99c5d32e0f37e1724e57
Yago <yagowns@gmail.com>
2019-04-02 22:37:29 +0100

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,8 +1,8 @@
 var audio2 = document.getElementById("emails");
-audio2.volume = 0.3;
+audio2.volume = 0.15;
 audio2.autoplay = false;
 
 
 var audio3 = document.getElementById("password");
-audio3.volume = 0.3;
+audio3.volume = 0.15;
 audio3.autoplay = false;
